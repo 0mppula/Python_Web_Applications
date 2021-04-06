a6c4567a83d9ad4a1c0a354fc9f0c767011c0f90
Omar Kraidie <omppula_1@hotmail.com>
2021-04-06 14:10:39 +0300

    Worked with shell

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -22,4 +22,7 @@ class Entry(models.Model):
 
     def __str__(self):
         """ Return a string representation of the model. """
-        return f'{self.text[:50]} ...'
+        if len(self.text) > 50:
+            return f'{self.text[:50]} ...'
+        else:
+            return self.text
