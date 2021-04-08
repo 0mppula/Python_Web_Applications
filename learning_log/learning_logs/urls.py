6a4a18eca52ee906eedc646e7f52d2128ad1e715
Omar Kraidie <omppula_1@hotmail.com>
2021-04-08 20:05:56 +0300

    Created topics page

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -4,8 +4,12 @@ from django.urls import path
 
 from . import views
 
-# app_name = learning_logs
+app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Show all topics
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic
+    path('topics/<int:topics_id/>', views.topic, name='topic'),
 ]
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 
 def index(request):
     """ The homepage for learning Log.  """
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """ Displays all topics. """
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """ Display a single topic and all its entries. """
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
