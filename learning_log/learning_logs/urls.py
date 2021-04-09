cfd4384cd335e67366ebc6149e78a34a62817ded
Omar Kraidie <omppula_1@hotmail.com>
2021-04-09 09:09:27 +0300

    Created topic page, added css to pages

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     # Show all topics
     path('topics/', views.topics, name='topics'),
     # Detail page for a single topic
-    path('topics/<int:topics_id/>', views.topic, name='topic'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
