30eaa5f777bec304909eed582312bd0d3f643b02
Omar Kraidie <omppula_1@hotmail.com>
2021-04-13 19:13:12 +0300

    Added new_entry.html

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('topics/<int:topic_id>/', views.topic, name='topic'),
     # New topic page
     path('new_topic', views.new_topic, name='new_topic'),
+    # New entry for a topic
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 ]
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 
 def index(request):
@@ -37,3 +37,22 @@ def new_topic(request):
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """ Add a new entry to a topic. """
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data submitted create a blank form
+        form = EntryForm()
+    else:
+        # POST data submitted; process data
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+    
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
