784ccf235ae644f698872b2bd1040327c44b369f
Omar Kraidie <omppula_1@hotmail.com>
2021-04-19 14:14:14 +0300

    Added css styles to learning logs

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -9,5 +9,6 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     # Login page
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), 
+    name='login'),
 ]
