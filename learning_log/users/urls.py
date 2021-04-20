60325456dd501b221905217fe95a83beb12e8fbf
Omar Kraidie <omppula_1@hotmail.com>
2021-04-20 11:25:36 +0300

    Created register page

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     name='login'),
     # Logout page
     path('logout/', views.logout_view, name='logout'),
+    # Registration page
+    path('register/' , views.register, name='register'),
 ]
