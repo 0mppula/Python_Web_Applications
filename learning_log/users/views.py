2a09c68b735c79d288903430d08427f79d339931
Omar Kraidie <omppula_1@hotmail.com>
2021-04-20 12:09:34 +0300

    1[A wx wq

--- learning_log/users/views.py
+++ learning_log/users/views.py
@@ -21,7 +21,8 @@ def register(request):
     if form.is_valid():
         new_user = form.save()
         # Log in user in and then redirect to home page
-        authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+        authenticated_user = authenticate(username=new_user.username, 
+            password=request.POST['password1'])
         login(request, authenticated_user)
         return HttpResponseRedirect(reverse('learning_logs:index'))
 
