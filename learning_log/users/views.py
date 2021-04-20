838886dd73694f3980045cc83e925e81494e98c9
Omar Kraidie <omppula_1@hotmail.com>
2021-04-20 11:39:36 +0300

    Fixed registration page bug

--- learning_log/users/views.py
+++ learning_log/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import logout, logout, authenticate
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
 def logout_view(request):
@@ -21,8 +21,7 @@ def register(request):
     if form.is_valid():
         new_user = form.save()
         # Log in user in and then redirect to home page
-        authenticated_user = authenticate(username=new_user.name, 
-            password=request.POST['password1'])
+        authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
         login(request, authenticated_user)
         return HttpResponseRedirect(reverse('learning_logs:index'))
 
