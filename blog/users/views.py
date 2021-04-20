f30389c602ad5e3ae2e16b00c9d509353dc35436
Omar Kraidie <omppula_1@hotmail.com>
2021-04-20 20:02:49 +0300

    Removed help text from registration page

--- blog/users/views.py
+++ blog/users/views.py
@@ -3,6 +3,16 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+class UserCreateForm(UserCreationForm):
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreateForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
 
 def logout_view(request):
     """ Logout user. """
@@ -13,18 +23,16 @@ def register(request):
     """ Register a new user. """
     if request.method != 'POST':
         # Display blank registeration form
-        form = UserCreationForm()
+        form = UserCreateForm()
     else:
         # Process completed form
-        form = UserCreationForm(data=request.POST)
+        form = UserCreateForm(data=request.POST)
 
     if form.is_valid():
         new_user = form.save()
-        print(f'New user: {new_user}')
         # Log the user in and redirect to home page
         authenticated_user = authenticate(username=new_user.username,
             password=request.POST['password1'])
-        print(f'Authenticated user: {authenticated_user}')
         login(request, authenticated_user)
         return HttpResponseRedirect(reverse('blogs:index'))
 
