8f6f501ce6a077d88daca6fc649fbffd07c8cf74
Omar Kraidie <omppula_1@hotmail.com>
2021-04-20 19:17:15 +0300

    Created logout page

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -3,9 +3,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     # Login page
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), 
         name='login'),
+    # Logout page
+    path('logout/', views.logout_view, name='logout'),
 ]
--- blog/users/views.py
+++ blog/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout
 
-# Create your views here.
+def logout_view(request):
+    """ Logout user. """
+    logout(request)
+    return HttpResponseRedirect(reverse('blogs:index'))
+
