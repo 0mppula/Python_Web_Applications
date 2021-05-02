889b51d489b9f0402fe7c9578d8b0ee19ce1322a
Omar Kraidie <omppula_1@hotmail.com>
2021-05-02 12:38:35 +0300

    Added config settings for live deployment with Heroku

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -132,7 +132,27 @@ STATICFILES_DIRS = [
 # My settings
 LOGIN_URL = '/users/login/'
 
-# Settings for django-bootstrap3
+# Settings for django-bootstrap5
 BOOTSTRAP5 = {
     'include_jquery': True,
 }
+
+# Heroku settings
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    
+    # Allow all host headers
+    ALLOWED_HOSTS = ['*']
+
+    # Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
