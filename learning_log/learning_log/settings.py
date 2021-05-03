4acc25e6507ec6cc04761afee7a46e5a0e08e419
Omar Kraidie <omppula_1@hotmail.com>
2021-05-03 15:41:36 +0300

    Separated static files from static server files

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'learning_log/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILE_DIRS = [ 
       "static/css", 
