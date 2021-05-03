4589a1e1fd58a837824a65ab20dbc9e10efa8a34
Omar Kraidie <omppula_1@hotmail.com>
2021-05-03 15:15:46 +0300

    Renamed live static files directory to server

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILE_DIRS = [ 
       "static/css", 
-      "staticfiles",
+      "server/staticfiles",
 ]
 
 # My settings
