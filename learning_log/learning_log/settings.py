6698a2850519d3aa06158fb60c0b56d49eb82a19
Omar Kraidie <omppula_1@hotmail.com>
2021-05-04 10:01:01 +0300

    Fixed URL configs for learning log

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'server/static')
 STATIC_URL = 'learning_log/'
 STATICFILE_DIRS = [ 
     #   'static/css', 
-      'server/staticfiles/',
+    #   'server/staticfiles/',
 ]
 
 LOGIN_URL = '/users/login/'
