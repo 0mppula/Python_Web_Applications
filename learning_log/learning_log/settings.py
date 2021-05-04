3bd4c7958050a71f09ec9a0d6720fd3df678223f
Omar Kraidie <omppula_1@hotmail.com>
2021-05-04 13:24:24 +0300

    Created folder directory for error pages

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -58,7 +58,8 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                os.path.join(BASE_DIR, 'templates/errors')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
